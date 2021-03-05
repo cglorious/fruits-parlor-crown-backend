3309da48f13ac1c552df15504ff0a56c19360d7e
Catherine Gloria <catherinegloria@gmail.com>
2021-03-04 21:17:36 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,7 +51,35 @@ beryl = Character.create(
 moon = Character.create(
   name: "Usagi Tsukino",
   title: "Sailor Moon",
-  power: "Generation of damaging energy Uses the power of Silver Crystal Healing powers Teleportation (with other Sailor Guardians)",
+  power: "Moon Prism Power, Moon Tiara Magic",
   affiliation: "Inner Sailor Scouts",
-  bio: "Initially believing herself to be an ordinary girl, she is later revealed to be the reincarnated form of the Princess of the Moon Kingdom, and she subsequently discovers her original name, Princess Serenity.",
+  bio: "Usagi is a careless fourteen-year-old girl with an enormous capacity for love, compassion, and understanding. Usagi transforms into the heroine called Sailor Moon, the Guardian of Love and Justice.",
+  category_id: guardian.id)
+mercury = Character.create(
+  name: "Ami Mizuno",
+  title: "Sailor Mercury",
+  power: "Mercury Power, Mercury Bubbles Blast",
+  affiliation: "Inner Sailor Scouts",
+  bio: "Ami is a quiet but intelligent fourteen-year-old bookworm in Usagi's class with a rumored IQ of 300. She can transform into Sailor Mercury, the Guardian of Water and Wisdom.",
+  category_id: guardian.id)
+mars = Character.create(
+  name: "Rei Hino",
+  title: "Sailor Mars",
+  power: "Mars Power, Mars Fire Ignite",
+  affiliation: "Inner Sailor Scouts",
+  bio: "Rei is an elegant fourteen-year-old miko (English: shrine maiden). Because of her work as a Shinto priestess, Rei has limited precognition and can dispel or nullify evil using special ofuda scrolls, even in her civilian form. She transforms into Sailor Mars, the Guardian of Fire and Passion.",
+  category_id: guardian.id)
+jupiter = Character.create(
+  name: "Makoto Kino",
+  title: "Sailor Jupiter",
+  power: "Jupiter Power, Jupiter Thunder Crash",
+  affiliation: "Inner Sailor Scouts",
+  bio: "Makoto is a fourteen-year-old girl who is a student in Usagi Tsukino's class and was rumoured to have been expelled from her previous school for fighting. Unusually tall and strong for a Japanese schoolgirl,[15]:12 she transforms into Sailor Jupiter, the Guardian of Thunder and Courage.",
+  category_id: guardian.id)
+venus = Character.create(
+  name: "Minako Aino",
+  title: "Sailor Venus",
+  power: "Venus Power, Venus Crescent Beam Smash",
+  affiliation: "Inner Sailor Scouts",
+  bio: "Minako is a fourteen-year-old perky dreamer. Minako first appears as the main protagonist of Codename: Sailor V. Minako transforms into Sailor Venus, the Guardian of Love and Beauty, and leads Sailor Moon's four inner Guardians, while acting as Sailor Moon's bodyguard and decoy because of their near-identical looks.",
   category_id: guardian.id)
